296f258c612ef792954f82b125c543a0f3680fa2
luis123delafuente <delafuentemartinez.2248290@studenti.uniroma1.it>
2026-04-06 10:25:10 +0200

    Corrección de error en asignacionService.js

--- frontend/src/services/asignacionService.js
+++ frontend/src/services/asignacionService.js
@@ -3,7 +3,13 @@ import axios from 'axios'
 const BASE_URL = 'http://localhost:8080/asignaciones'
 
 export const obtenerResultados = () =>
-  axios.get(`${BASE_URL}`)
+  axios.get(⁠ ${BASE_URL} ⁠)
 
 export const procesarAsignaciones = () =>
-  axios.post(`${BASE_URL}/procesar`)
+  axios.post(⁠ ${BASE_URL}/procesar ⁠)
+
+export const obtenerMiAsignacion = (usuarioId) =>
+  axios.get(⁠ ${BASE_URL}/estudiante/${usuarioId} ⁠)
+
+export const obtenerResultadoEstudiante = (usuarioId) =>
+  axios.get(⁠ ${BASE_URL}/estudiante/${usuarioId} ⁠)
--- frontend/src/services/ofertaService.js
+++ frontend/src/services/ofertaService.js
@@ -5,5 +5,3 @@ const BASE_URL = 'http://localhost:8080/ofertas'
 export const publicarOferta = (usuarioId, grado, plazas) =>
   axios.post(BASE_URL, { usuarioId, grado, plazas })
 
-export const obtenerOfertasMiUniversidad = (usuarioId) =>
-  axios.get(⁠ ${BASE_URL}/mi-universidad ⁠, { params: { usuarioId } })
